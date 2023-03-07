2461a85ed71c0a112c6185dda6d0b4542b79371f
softpug1023 <jxie32@ucsc.edu>
2023-03-07 12:06:57 +0800

    swift first push

---
+++ swift/HelloWorldSwift/DBRSample_Bridging_Header.h
@@ -0,0 +1,7 @@
+#ifndef DCESample_Bridging_Header_h
+#define DCESample_Bridging_Header_h
+
+#import <DynamsoftBarcodeReader/DynamsoftBarcodeReader.h>
+#import <DynamsoftCameraEnhancer/DynamsoftCameraEnhancer.h>
+
+#endif
